45421d90750e37f113bf2c30bb22cf09d9559e45
System Administrator <root@chenmatoMacBook-Air.local>
2017-04-27 18:04:53 +0800

    项目入口

---
+++ app.js
@@ -0,0 +1,33 @@
+const Koa = require('koa')
+const views = require('koa-views')
+const path = require('path')
+const static = require('koa-static')
+const convert = require('koa-convert')
+const bodyParser = require('koa-bodyparser')
+const app = new Koa()
+const session = require('koa-session')
+const fs = require('fs')
+
+global.moment = require('moment')
+app.keys = ['secret key']
+
+let CONFIG = {
+	key: 'blog',
+	maxAge: 1000000,
+	overwrite: true,
+	httpOnly: true,
+	signed: true,
+}
+app.use(convert(session(CONFIG, app)))
+
+app.use(convert(static(path.join(__dirname, 'public'))))
+app.use(convert(static(path.join(__dirname, 'node_modules'))))
+app.use(bodyParser())
+app.use(views(path.join(__dirname, 'views/pages'), {
+	extension: 'pug'
+}))
+
+const router = require('./router/router')
+
+app.use(router.routes()).use(router.allowedMethods())
+app.listen(3000)
